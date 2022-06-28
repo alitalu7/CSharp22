b14eba3b05b4daf01c641ca2a27d7a65d7e790a9
alitalu7 <55026802+alitalu7@users.noreply.github.com>
2022-06-28 09:33:36 +0300

    Add project files.

---
+++ CSharp22/Program.cs
@@ -0,0 +1,47 @@
+﻿byte i;
+
+Console.WriteLine("1'den 4'e kadar gün veya departman seçin");
+i = Convert.ToByte(Console.ReadLine());
+
+Gunler gunAdi;
+gunAdi = (Gunler)i;
+Departman departman;
+departman = (Departman)i;
+
+
+
+if (i==0)
+{
+    Console.WriteLine("Toplantı {1} günü {0} birimi ile olacak", (Departman)i, (Gunler)i);
+}
+else if (i==1)
+{
+    Console.WriteLine("Toplantı {1} günü {0} birimi ile olacak", (Departman)i, (Gunler)i);
+}
+else if (i == 2)
+{
+    Console.WriteLine("Toplantı {1} günü {0} birimi ile olacak", (Departman)i, (Gunler)i);
+}
+else if (i == 3)
+{
+    Console.WriteLine("Toplantı {1} günü {0} birimi ile olacak", (Departman)i, (Gunler)i);
+}
+else if (i == 4)
+{
+    Console.WriteLine("Toplantı {1} günü {0} birimi ile olacak", (Departman)i, (Gunler)i);
+}
+else
+{
+    Console.WriteLine("Haftasonu toplantı çalışma yoktur.");
+}
+
+enum Departman
+{
+    Elektrik, Mekanik, Otomasyon, Bilgi_Islem, Laboratuvar
+}
+
+enum Gunler
+{
+    Pazartesi, Sali, Carsamba, Persembe, Cuma, Cumartesi, Pazar
+}
+
